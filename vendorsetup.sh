fc09852b6234dd3175320c85164ea627cf13818a
Fechinator <fechter73@googlemail.com>
2014-11-26 15:08:35 +0100

    add d855

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,3 +4,4 @@ add_lunch_combo du_grouper-userdebug
 add_lunch_combo du_hammerhead-userdebug
 add_lunch_combo du_mako-userdebug
 add_lunch_combo du_shamu-userdebug
+add_lunch_combo du_d855-userdebug
